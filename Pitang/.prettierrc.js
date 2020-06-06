d17ad339b0050b2d2482457ef07c4afe3e81a379
Nikollas Betiol <nikollasdev@gmail.com>
2020-06-06 14:53:15 -0400

    feat: add code transform

--- Pitang/.prettierrc.js
+++ Pitang/.prettierrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
+    bracketSpacing: true,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'all',
 };
