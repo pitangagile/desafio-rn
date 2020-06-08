76c019ace4ee6485501b79b5c3cd2fc9f0c7a62a
Nikollas Betiol <nikollasdev@gmail.com>
2020-06-08 16:39:07 -0400

    fix: sizes

--- Pitang/src/helpers/utils.ts
+++ Pitang/src/helpers/utils.ts
@@ -2,10 +2,10 @@ import { Dimensions } from 'react-native';
 
 const sizes = () => {
   const windowWidth = Dimensions.get('window').width;
-  if (windowWidth === 320) {
+  if (windowWidth <= 320) {
     return 'small';
   }
-  if (windowWidth === 414) {
+  if (windowWidth >= 414) {
     return 'large';
   }
   return 'medium';
